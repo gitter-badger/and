ca0f5bedb091ff2e74b72ae9697e639b9f8a1a34
Ertuğrul Seyhan <hi@ertugrulseyhan.com>
2015-12-25 09:10:30 +0200

    Create project

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright 2015 Ertuğrul Seyhan. All rights reserved. See License file in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("And")]
+[assembly: AssemblyDescription("Experimental Programming language project")]
+[assembly: AssemblyTrademark("Ertuğrul Seyhan")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Ertuğrul Seyhan")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
