6c2e3a4f7073e49ab24ba242f2f8887842d1e8fe
Ertuğrul Seyhan <hi@ertugrulseyhan.com>
2015-12-25 11:03:19 +0200

    Base class for Parser & AST

---
+++ Source/Parser/AstNode.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AstNode.cs" author="Ertuğrul Seyhan">
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace And
+{
+    using System.Collections.Generic;
+
+    public abstract class AstNode
+    {
+        public LinkedList<AstNode> Children { get; private set;}
+
+        protected AstNode()
+        {
+            Children = new LinkedList<AstNode>();
+        }
+    }
+}
---
+++ Source/Parser/Parser.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Parser.cs" author="Ertuğrul Seyhan">
+//   Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace And
+{
+    using System.Collections.Generic;
+
+    public class Parser
+    {
+        private readonly LinkedList<Token> _tokens;
+        public int Position;
+
+        public Parser(LinkedList<Token> tokens) {
+            _tokens = tokens;
+        }
+    }
+}
